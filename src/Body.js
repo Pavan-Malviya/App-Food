7baf9a1f99987a8276775064c471eb4710eb6124
Pavan <pavanmalviya2@gmail.com>
2024-01-05 18:08:59 +0530

    new changes

--- src/Body.js
+++ src/Body.js
@@ -1,64 +1,52 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IMG_CDN_URL } from "./constants";
-import { restaurantList } from "./constants";
 import Shimmer from "./Shimmer";
-import { useState } from "react";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { swiggy_api_URL } from "./constants";
-
-function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-  );
-  return filterData;
-}
 
 const Body = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filterRestaurants, setFilterRestaurants] = useState([]);
-  // const [errorMessage, setErrorMessage] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-  const [searchText, setsearchText] = useState("");
-
-  //empty dependency array ==> once after render
-  //dep array [searchText] ==> once after initial render
   useEffect(() => {
-    //API call
     getRestaurant();
   }, []);
 
   async function getRestaurant() {
-    const data = await fetch(swiggy_api_URL);
-    // https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.7131695&lng=76.5650829&page_type=DESKTOP_WEB_LISTING
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.7131695&lng=76.5650829&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch: ${response.status} ${response.statusText}`
+        );
+      }
 
-    const json = await data.json();
-    console.log(json);
-    setAllRestaurants(json.data?.cards[2]?.data?.data?.cards);
-    setFilterRestaurants(json.data?.cards[2]?.data?.data?.cards);
+      const data = await response.json();
+      console.log(data);
+      const cardsData =
+        data?.data?.cards[1]?.card?.card.gridElements.infoWithStyle.restaurants;
+      console.log("cards data", cardsData);
+      if (cardsData) {
+        // Set allRestaurants to the first 12 cards in the array
+        setAllRestaurants(cardsData.slice(0, 12));
+
+        // Set filterRestaurants to the entire cards array
+        setFilterRestaurants(cardsData.slice(0, 12));
+
+        // Now you can use allRestaurants and filterRestaurants as per your requirements
+      }
+      // setAllRestaurants(data?.data?.cards[0]?.data?.data?.cards);
+      // setFilterRestaurants(data?.data?.cards[0]?.data?.data?.cards);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
-
-  // use searchData function and set condition if data is empty show error message
-  // const searchData = (searchText, restaurants) => {
-  //   if (searchText !== "") {
-  //     const data = filterData(searchText, restaurants);
-  //     setFilterRestaurants(data);
-  //     setErrorMessage("");
-  //     if (data.length === 0) {
-  //       <h4>No match</h4>;
-  //     }
-  //   } else {
-  //     setErrorMessage("");
-  //     setFilterRestaurants(restaurants);
-  //   }
-  // };
-
-  // if allRestaurants is empty don't render restaurants cards
-  // if (!allRestaurants) return null;
 
   return (
     <div className="p-4 m-4">
-
       <div className="flex justify-center gap-2">
         <input
           className="rounded-md bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 px-4 text-white border-none"
@@ -66,34 +54,35 @@ const Body = () => {
           placeholder="search"
           value={searchText}
           onChange={(e) => {
-            setsearchText(e.target.value);
+            setSearchText(e.target.value);
           }}
-        ></input>
+        />
         <button
-          className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500  rounded-full p-1 text-white "
+          className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-full p-1 text-white"
           onClick={() => {
-            //need to filter the data
-            //here filterData took variable (searchText, restaurants)
-            const data = filterData(searchText, allRestaurants);
-            //update the state of the restaurants
-            setFilterRestaurants(data);
+            const filteredData = allRestaurants.filter((restaurant) =>
+              restaurant?.info?.name
+                ?.toLowerCase()
+                ?.includes(searchText.toLowerCase())
+            );
+            setFilterRestaurants(filteredData);
           }}
         >
           Search
         </button>
-
-
       </div>
 
-      {allRestaurants.length === 0 ? (
-        <Shimmer />
-      ) : (
-        <div className="flex justify-center flex-wrap w-full p-4 m-1">
-          {filterRestaurants.map((restaurant) => {
-            return <Link to={"/restaurant/" + restaurant.data.id} key={restaurant.data.id} style={{ textDecoration: "none" }}> <Card {...restaurant.data} /></Link>;
-          })}
-        </div>
-      )}
+      <div className="flex justify-center flex-wrap w-full p-4 m-1">
+        {filterRestaurants.map((restaurant) => (
+          <Link
+            to={"/restaurant/" + restaurant.info.id}
+            key={restaurant.info.id}
+            style={{ textDecoration: "none" }}
+          >
+            <Card {...restaurant.info} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
@@ -106,19 +95,21 @@ const Card = ({
   slaString,
 }) => {
   return (
-
-    <div className="max-w-xs h-75 m-3 p-6  rounded-md shadow-md bg-gray-900 text-gray-50">
-      <img src={IMG_CDN_URL + cloudinaryImageId} alt="" className="object-cover object-center w-full rounded-md h-60 dark:bg-gray-500" />
+    <div className="max-w-xs h-75 m-3 p-6 rounded-md shadow-md bg-gray-900 text-gray-50">
+      <img
+        src={IMG_CDN_URL + cloudinaryImageId}
+        alt=""
+        className="object-cover object-center w-full rounded-md h-60 dark:bg-gray-500"
+      />
       <div className="mt-6 mb-2">
-        <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">{locality}</span>
+        <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">
+          {locality}
+        </span>
         <h2 className="text-xl font-semibold tracking-wide">{name}</h2>
       </div>
-      <p className="dark:text-gray-100">{slaString} </p>
+      <p className="dark:text-gray-100">{slaString}</p>
     </div>
-
   );
-
-
 };
 
 export default Body;
--- src/RestaurantMenu.js
+++ src/RestaurantMenu.js
@@ -1,59 +1,69 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "./constants";
 import Shimmer from "./Shimmer";
 import { swiggy_menu_api_URL } from "./constants";
 
-
-
 const RestaurantMenu = () => {
-    const params = useParams();
-    const { id } = params;
+  const params = useParams();
+  const { id } = params;
 
+  const [restaurantMenu, setRestaurantMenu] = useState(null);
 
-    const [restaurantmenu, setRestaurantMenu] = useState(null);
+  useEffect(() => {
+    getRestaurantInfo();
+  }, []);
 
-    useEffect(() => {
-        getRestaurantInfo();
-    }, []);
+  async function getRestaurantInfo() {
+    const response = await fetch(swiggy_menu_api_URL + id);
+    const restaurantData = await response.json();
+    console.log(restaurantData);
+    setRestaurantMenu(restaurantData?.data.cards[0]?.card?.card);
+    console.log(restaurantData?.data.cards[0]?.card?.card);
+    // Adjust this based on the actual structure
+  }
 
-    async function getRestaurantInfo() {
-        const data = await fetch(swiggy_menu_api_URL + id);
-        const json = await data.json();
-        console.log(json);
-        setRestaurantMenu(json.data);
-
-    }
-
-
-    return (!restaurantmenu) ? <Shimmer /> : (
-        <div>
-            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-                <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 p-5 m-4 rounded-md">
-                    <img src={IMG_CDN_URL + restaurantmenu.cloudinaryImageId} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-800 p-6" />
-                    <div className="p-6 space-y-2 lg:col-span-5 text-gray-100">
-                        <h3 className="text-2xl font-semibold sm:text-4xl">{restaurantmenu.name}</h3>
-                        <span className="text-xs dark:text-gray-200">{restaurantmenu.cuisines.join(" , ")}</span>
-                        <p>{restaurantmenu.area}</p>
-                        <p>{restaurantmenu.costForTwoMsg}</p>
-                        <p>{restaurantmenu.avgRating}</p>
-                    </div>
-                </a>
-            </div>
-            <div className="flex justify-center">
-                <div className="p-2 m-2">
-                    <h1 className="font-bold">Menu</h1>
-                    <ul className="menu bg-base-100 w-56 h-56 p-2 rounded-box" style={{ listStyleType: "none" }}>
-                        {
-                            Object.values(restaurantmenu.menu.items).map((item) => <li className="p-1 m-2" key={item.id}>{item.name}</li>)
-                        }
-                    </ul>
-                </div>
-            </div>
+  return !restaurantMenu ? (
+    <Shimmer />
+  ) : (
+    <div>
+      <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+        <div className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 p-5 m-4 rounded-md">
+          <img
+            src={IMG_CDN_URL + restaurantMenu.info.cloudinaryImageId}
+            alt=""
+            className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-800 p-6"
+          />
+          <div className="p-6 space-y-2 lg:col-span-5 text-gray-100">
+            <h3 className="text-2xl font-semibold sm:text-4xl">
+              {restaurantMenu.info.name}
+            </h3>
+            <span className="text-xs dark:text-gray-200">
+              {restaurantMenu.info.cuisines.join(" , ")}
+            </span>
+            <p>{restaurantMenu.info.areaName}</p>
+            <p>{restaurantMenu.info.costForTwoMessage}</p>
+            <p>{restaurantMenu.info.avgRating}</p>
+          </div>
         </div>
-
-
-    );
+      </div>
+      <div className="flex justify-center">
+        <div className="p-2 m-2">
+          <h1 className="font-bold">Menu</h1>
+          <ul
+            className="menu bg-base-100 w-56 h-56 p-2 rounded-box"
+            style={{ listStyleType: "none" }}
+          >
+            {restaurantMenu.info.cuisines.map((cuisine, index) => (
+              <li className="p-1 m-2" key={index}>
+                {cuisine}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantMenu;
